b9aabb7f2cadda8b097dd5fd5d9e88a345be6da0
spacecowboi <cunningham2192@gmail.com>
2020-01-07 15:54:25 -0800

    Saving changes with finished JS questions

--- project-directory/js/app.js
+++ project-directory/js/app.js
@@ -1,4 +1,35 @@
 'use strict';
 
 var greeting = prompt('Welcome to my page! What is your name?');
-    console.log('The user put in + greeting')
+console.log('The user put in + greeting');
+
+
+var question1 = prompt('What do you think my name is?');
+if(question1.toUpperCase() === 'ANTHONY'){
+  alert('Correct!');
+} else
+  alert('It is anthony!');
+
+var question2 = prompt('Did I grow up in the US?');
+if(question2 .toLowerCase() === 'yes'){
+  alert('Correct!');
+} else
+  alert('I did actually!');
+
+var question3 = prompt('Do I like scary movies?');
+if(question3.toLowerCase() === 'yes'){
+  alert('Take my strong hand!');
+} else
+  alert('I LOVE scary movies');
+
+var question4 = prompt('Is my favorite food Japanese?');
+if(question4.toUpperCase() === 'YES'){
+  alert('I love sushi');
+} else
+  alert('It is Japanese!');
+
+var question5 = prompt('Do I like video games?');
+if(question5.toLowerCase() === 'yes'){
+  alert('Correct! I love them!');
+} else
+  alert('wrongo! I love games!');
