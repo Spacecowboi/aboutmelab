d6bed952663e6406ccfb1084d5ce33832adf5e7d
spacecowboi <cunningham2192@gmail.com>
2020-01-07 20:32:26 -0800

    added boxes and color to body of texts, re-added console logs to JS variables

--- project-directory/js/app.js
+++ project-directory/js/app.js
@@ -7,30 +7,35 @@ alert('Good to meet you ' + greeting + '!');
 
 var question1 = prompt('What do you think my name is?');
 if(question1.toUpperCase() === 'ANTHONY'){
+  //console.log(question1)
   alert('Correct!');
 } else
   alert('It is anthony!');
 
 var question2 = prompt('Did I grow up in the US?');
 if(question2 .toLowerCase() === 'yes'){
+  //console.log(question2)
   alert('Correct!');
 } else
   alert('I did actually!');
 
 var question3 = prompt('Do I like scary movies?');
 if(question3.toLowerCase() === 'yes'){
+  //console.log(question3)
   alert('Take my strong hand!');
 } else
   alert('I LOVE scary movies');
 
 var question4 = prompt('Is my favorite food Japanese?');
 if(question4.toUpperCase() === 'YES'){
+  //console.log(question4)
   alert('I love sushi');
 } else
   alert('It is Japanese!');
 
 var question5 = prompt('Do I like video games?');
 if(question5.toLowerCase() === 'yes'){
+  //console.log(question5)
   alert('Correct! I love them!');
 } else
   alert('wrongo! I love games!');
